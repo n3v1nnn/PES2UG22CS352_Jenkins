a8a45dc2f14f93c7ca7cd62c2ca41c6a6be7a034
Nevin Nethr Suresh Marthandan <nnm@Nevins-MacBook-Air.local>
2025-03-13 15:09:04 +0530

    Updated hello.cpp and added Dockerfile

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run this  \n";
+  cout << "I have successfully built and run this  \n";                     
+  cout << "PES2UG22CS352\n";
   return 0;
 }
